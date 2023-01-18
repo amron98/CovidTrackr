f29fa2d2f2f67d670d1c844e8aee57c3d77f3279
Amron <amron98@gmail.com>
2023-01-17 16:53:57 -0800

    Initial Commit

---
+++ CovidTrackr/CovidTrackrApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidTrackrApp.swift
+//  CovidTrackr
+//
+//  Created by Amron B on 1/17/23.
+//
+
+import SwiftUI
+
+@main
+struct CovidTrackrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
