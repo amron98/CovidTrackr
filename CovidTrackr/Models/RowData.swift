6d4ac0623bea6e51bc3c72d884a599cc125fa681
Amron <amron98@gmail.com>
2023-01-23 18:44:11 -0800

    Implemented countries list row data model

---
+++ CovidTrackr/Models/RowData.swift
@@ -0,0 +1,16 @@
+//
+//  RowData.swift
+//  CovidTrackr
+//
+//  Created by Amron B on 1/22/23.
+//
+
+import Foundation
+
+struct RowData {
+    let country: String
+    let confirmed: String
+    let deaths: String
+    let flag: String
+}
+
