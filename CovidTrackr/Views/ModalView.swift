917ac069c03aadb66bb7a2434631f62809cb00ea
Amron <amron98@gmail.com>
2023-01-27 00:48:49 -0800

    Implemented ModalView with samle data

---
+++ CovidTrackr/Views/ModalView.swift
@@ -0,0 +1,28 @@
+//
+//  ModalView.swift
+//  CovidTrackr
+//
+//  Created by Amron B on 1/24/23.
+//
+
+import SwiftUI
+
+struct ModalView: View {
+
+    var country: CountryData
+    var timeline: Timeline
+    
+    init(country: CountryData, timeline: Timeline) {
+        self.country = country
+        self.timeline = timeline
+    }
+    
+    var body: some View {
+        Text("\(country.country!) clicked")
+        LineChartView(title: country.country!, timeline: timeline)
+            .padding()
+    }
+        
+}
+
+
