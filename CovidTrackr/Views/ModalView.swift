3addb606fee1da4abba167e9d92aa5d0a73567f3
Amron <amron98@gmail.com>
2023-02-15 14:45:23 -0800

    Update modal view to dispay country info and timeline

--- CovidTrackr/Views/ModalView.swift
+++ CovidTrackr/Views/ModalView.swift
@@ -9,11 +9,57 @@ import SwiftUI
 
 struct ModalView: View {
 
-    var country: Country
-    var timeline: Timeline
+    @ObservedObject var viewModel: ModalViewModel
+    @Environment(\.dismiss) private var dismiss
     
     var body: some View {
-        Text("\(country.name) clicked")
+        VStack {
+            // Close Button
+            HStack {
+                Spacer()
+                Button(
+                    action: dismiss.callAsFunction,
+                    label: {
+                        Image(systemName: "xmark.circle.fill")
+                            .foregroundColor(.secondary)
+                            .font(.title)
+                    }
+                )
+            }
+            
+            // Country Info
+            VStack{
+                Text(
+                    Utils
+                        .getFlag(from: "\(viewModel.country.info?.iso2 ?? "🏁")")
+                )
+                    .font(.custom("Arial", size: 48))
+                
+                
+                Text(viewModel.country.name)
+                    .font(.title)
+                
+                
+                Text(viewModel.country.continent ?? "")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+            }
+            
+            // Country Timeline
+            VStack {
+                if viewModel.isLoading {
+                    Spacer()
+                    ProgressView()
+                    Spacer()
+                } else if (!viewModel.country.timeline.cases!.isEmpty) {
+                    // display chart view
+                    LineChartView(title: viewModel.country.name, timeline: $viewModel.country.timeline)
+                } else {
+                    Text("No data available.")
+                }
+            }
+        }
+        .padding()
     }
         
 }
